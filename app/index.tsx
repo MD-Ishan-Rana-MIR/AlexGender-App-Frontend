6105d9df21f7a418b91d37f015f70ab7db6ceebc
ishanrana363 <ishanrana363@gmail.com>
2025-12-23 12:22:33 +0600

    login page completed

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import Login from '@/components/authentication/login/Login'
+import React from 'react'
+
+const index = () => {
+    return (
+        <Login />
+    )
+}
+
+export default index
+
---
+++ app/registration.tsx
@@ -0,0 +1,13 @@
+import Registration from '@/components/authentication/registration/Registration'
+import React from 'react'
+import { StyleSheet } from 'react-native'
+
+const registration = () => {
+    return (
+        <Registration />
+    )
+}
+
+export default registration
+
+const styles = StyleSheet.create({})
